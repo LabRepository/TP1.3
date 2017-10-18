977e60e0218f25fb9674ff10daf1c4551e5ad4a2
WeronikaJasiak <32877582+WeronikaJasiak@users.noreply.github.com>
2017-10-18 07:26:43 +0200

    Add files via upload

---
+++ DeckTest.java
@@ -0,0 +1,38 @@
+import junit.framework.TestCase;
+
+public class DeckTest extends TestCase {
+	private Deck deck;
+	protected void setUp() throws Exception {
+        super.setUp();
+        deck = new Deck(52);
+    }
+	public void testFirstCardEqualsAce() throws Exception {
+		setUp();
+		Card card = new Card(0,0);
+		assertEquals(deck.getFirstCard(),card.toString());
+	}
+	
+	public void testDesckIsNotNull() throws Exception {
+		setUp();
+		assertNotNull(deck);
+	}
+	
+	public void testConstructors() throws Exception {
+		setUp();
+		Deck deck1 = new Deck(24);
+		assertNotSame(deck, deck1);
+	}
+	
+	public void testConstructors2() throws Exception {
+		setUp();
+		Deck deck1 = new Deck(52);
+		assertFalse(deck == deck1);
+	}
+	
+	public void testAfterShuffleAndTestDeckExist() throws Exception {
+		setUp();
+		deck.shuffle();
+		deck.sort();
+		assert(deck != null);
+	}
+}
